81c2d2ea048048d60a9ace08d69c50cacf5e3913
Shaurya Jha <shauryajha35@gmail.com>
2024-11-16 20:47:01 +0530

    Add Server Init

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import jwt from "jsonwebtoken"
+
+const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+
